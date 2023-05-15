60ed031782bab87d3108484b8d96412e9cd82cdf
Angel Naibaho <101439456+angelnaibaho@users.noreply.github.com>
2023-05-15 20:29:55 +0700

    Add files via upload

---
+++ Praktikum 8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+    <center>
+	<h2>Login</h2>
+	<form action="ceklogin.php" method="POST">
+		<label>Username:</label>
+		<input type="text" name="username"><br> <br>
+
+		<label>Password:</label>
+		<input type="password" name="password"><br>
+        <br>
+		<input type="submit" value="Login">
+	</form>
+    </center>
+</body>
+</html>
